0691f70b0365d6f0dde87a157c959d73d7b424e3
hakjaelee <james.lee@eromict.com>
2018-08-17 17:38:49 +0900

    Initial commit

---
+++ src/main/java/com/test/decrypt/DecryptApplication.java
@@ -0,0 +1,15 @@
+package com.test.decrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class DecryptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecryptApplication.class, args);
+    }
+}
